d5dee9e5a6c6d3d0a517b8eaeaf718c581c93741
nguyen-tich-duy <duy.nguyen@classcom.app>
2021-06-23 23:58:45 +0700

    update ajax

--- public/assets/js/resignter.js
+++ public/assets/js/resignter.js
@@ -21,59 +21,43 @@ $("[name='password']").on("change", () => {
     .get(0)
     .setCustomValidity(passwordConfirm != passwordValue ? true : "");
 });
-
 function validatePhone(phone) {
   const regex = new RegExp("^[0-9]{6,15}$");
   return regex.test(phone);
 }
-
 $("[name='phone'").on("change", () => {
   var phone = $("[name='phone'").val();
   $("[name='phone'")
     .get(0)
     .setCustomValidity(!validatePhone(phone) ? true : "");
 });
-$(".validate-form").on("submit", (e) => {
-  var phone = $("[name='phone'").val();
+$("#register-form").on("submit", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+  const form = $("#register-form").get(0);
+  form.classList.add("was-validated");
+  var phone = $("[name='phone']").val();
   if (!validatePhone(phone)) {
-    e.preventDefault();
-    e.stopPropagation();
     $("[name='phone'").get(0).setCustomValidity(true);
   }
-});
-$(".validate-form").ajaxForm({
-  url: "/signin", // or whatever
-  dataType: "json",
-  success: function (data) {
-    window.parent.location.href = data.redirect;
-  },
-  error: (res) => {
-    if (res.status === 400) {
-      $("[name='phone']").get(0).setCustomValidity(true);
-    } else if (res.status === 422) {
-      $("[name='password-repeat']").get(0).setCustomValidity(true);
-    }
-  },
-});
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  "use strict";
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add("was-validated");
+  console.log("form.checkValidity()", form.checkValidity());
+  if (form.checkValidity()) {
+    const phone = $("[name='phone']").val();
+    const name = $("[name='name']").val();
+    const password = $("[name='password']").val();
+    console.log("{ phone, name, password }", { phone, name, password });
+    $.ajax({
+      type: "POST",
+      data: { phone, name, password, "password-repeat": password },
+      url: "/signin", // or whatever
+      dataType: "json",
+      success: function (data) {
+        window.parent.location.href = data.redirect;
       },
-      false
-    );
-  });
-})();
+      error: (error) => {
+        console.log(error);
+        $("[name='phone']").get(0).setCustomValidity(true);
+      },
+    });
+  }
+});
